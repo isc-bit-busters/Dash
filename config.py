7063af420e9633e39d57bde344766667793b7f50
Célien Muller <muller.celien@deverage.ch>
2025-04-30 14:08:19 +0200

    clean mqtt topics

--- config.py
+++ config.py
@@ -2,14 +2,11 @@ MQTT_BROKER = "192.168.88.253"
 MQTT_PORT = 1883
 MQTT_TOPICS = [
     ("gate/ir", 0),
-    ("gate1/ir", 0),
-    ("gate2/ir", 0),
-    ("gate3/ir", 0),
-    ("gate4/ir", 0),
     ("gate1/start", 0),
     ("gate2/start", 0),
     ("gate1/finish", 0),
-    ("gate2/finish", 0)
+    ("gate2/finish", 0),
+    ("gate/mac_config/ack", 0)
 ]
 
 MAC_FILE = "mac_addresses.json"
